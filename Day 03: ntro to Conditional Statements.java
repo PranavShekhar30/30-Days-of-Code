6351bf8fd598d59a9706637892ae5bd771a04b34
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2022-01-11 11:10:18 -0800

    Create Day 03: ntro to Conditional Statements.java

---
+++ Day 03: ntro to Conditional Statements.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        if(N%2 != 0){
+            System.out.println("Weird");
+        }
+        else if(N%2 == 0 && N>=6 && N<=20){
+            System.out.println("Weird");
+        }
+        else{
+            System.out.println("Not Weird");
+        }
+        bufferedReader.close();
+    }
+}
