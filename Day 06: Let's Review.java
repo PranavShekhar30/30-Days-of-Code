7875210861ad026d70fee5842d4a51175764d4a2
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2022-01-16 11:29:05 -0800

    Update Day 06: Let's Review.java

--- Day 06: Let's Review.java
+++ Day 06: Let's Review.java
@@ -1,10 +1,13 @@
- public static void main(String[] args) {
+ public class Solution {
+
+    public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
         
         s.nextLine();
-         
+        
+        
         for(int i=0; i<N; i++){
             String S = s.nextLine();
             char[] charArray = S.toCharArray();
@@ -13,16 +16,21 @@
                 if(j%2 == 0){
                     System.out.print(charArray[j]);
                 }
+            
             }
             System.out.print(" ");
-             for (int j = 0; j < charArray.length; j++) {
-                if (j % 2 != 0) {
+            
+            
+            for(int j=0; j<charArray.length; j++){
+            
+                if(j%2 != 0){
                     System.out.print(charArray[j]);
                 }
+            
             }
-
-            System.out.println();
+             System.out.println();
+            }
         
-        }
+       s.close();
     }
 }
