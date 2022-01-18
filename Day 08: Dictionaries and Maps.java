49e9cd15a46549633fa2e53d9ad670a24a848aaa
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2022-01-18 11:06:32 -0800

    Create Day 08: Dictionaries and Maps.java

---
+++ Day 08: Dictionaries and Maps.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            map.put(name, phone);
+            
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if(map.get(s) != null){
+                System.out.println(s + "=" + map.get(s));
+            }
+            else{
+                 System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
