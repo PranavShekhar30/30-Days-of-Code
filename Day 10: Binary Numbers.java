3ecb3296c905c3e342cf97e2d4595dfca6b228cf
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2022-01-21 11:27:12 -0800

    Update Day 10: Binary Numbers.java

--- Day 10: Binary Numbers.java
+++ Day 10: Binary Numbers.java
@@ -1,1 +1,27 @@
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        int count = 0;
+        int max = 0;
+        
+        while(n != 0){
+            
+            if(n % 2 == 1){
+                count++;
+                if(count >= max){
+                    max = count;
+                }
+            }
+            else{
+                count = 0;
+            }
+            n = n/2;
+            
+        }
+        System.out.print(max);
+
+        bufferedReader.close();
+    }
+}
